cbc011ee497a335a5802f193f3b0f5171182c49d
Ojas Capital Limited <166379971+ojascapuk@users.noreply.github.com>
2024-07-06 14:58:07 +0100

    Update common_types.ts
    Updated the file with Comments

--- src/common_types.ts
+++ src/common_types.ts
@@ -1,30 +1,50 @@
+/*
+This code defines several TypeScript types, interfaces, and enumerations related to financial and commodity data.
+The PriceBar interface represents a price data point with a timestamp and closing price. PriceFeedSource is a union type indicating 
+the source of price data. CFTCCommodityCode, CFTCSubgroupName, and CFTCContractMarketCode are type aliases for strings representing various 
+CFTC-related identifiers. The IPriceFeed interface defines the structure for a price feed object, 
+including optional transformation functions. Finally, CFTCReportType and CFTCCommodityGroupType are enums 
+representing different report and commodity group types, respectively.
+
+
+*/
 export interface PriceBar {
-    timestamp: Date
-    close: number
+    timestamp: Date;
+    close: number;
 }
+// Defines an interface for a PriceBar, which includes a timestamp and a closing price.
 
-export type PriceFeedSource = 'FRED' | 'unknown'
+export type PriceFeedSource = 'FRED' | 'unknown';
+// Defines a type for the source of the price feed, which can be either 'FRED' or 'unknown'.
 
-export type CFTCCommodityCode = string
+export type CFTCCommodityCode = string;
+// Defines a type alias for CFTC Commodity Code as a string.
 
-export type CFTCSubgroupName = string
+export type CFTCSubgroupName = string;
+// Defines a type alias for CFTC Subgroup Name as a string.
 
 export interface IPriceFeed {
-    source: PriceFeedSource
-    name: string
-    symbol: string
-    transforms?: Function[]
+    source: PriceFeedSource;
+    name: string;
+    symbol: string;
+    transforms?: Function[];
 }
+// Defines an interface for a price feed, which includes the source, name, symbol, and optionally, an array of transformation functions.
+
 export enum CFTCReportType {
     FinancialFutures,
     Disaggregated,
     Legacy,
 }
+// Defines an enumeration for different types of CFTC reports.
 
 export enum CFTCCommodityGroupType {
     Financial = 'FINANCIAL INSTRUMENTS',
     NaturalResources = 'NATURAL RESOURCES',
     Agriculture = 'AGRICULTURE',
 }
+// Defines an enumeration for different types of CFTC commodity groups.
 
-export type CFTCContractMarketCode = string
+export type CFTCContractMarketCode = string;
+// Defines a type alias for CFTC Contract Market Code as a string.
+
